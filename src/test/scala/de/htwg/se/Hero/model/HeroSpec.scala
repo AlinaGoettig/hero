a082e66d2fdf83376897552bc38c4d72d5335c1d
ShuraBlack <shurablack38@gmail.com>
2020-11-05 17:45:29 +0100

    Coverage for Hero.scala

--- src/test/scala/de/htwg/se/Hero/model/HeroSpec.scala
+++ src/test/scala/de/htwg/se/Hero/model/HeroSpec.scala
@@ -1,12 +1,25 @@
 package de.htwg.se
 
 import de.htwg.se.Hero.model.Hero._
+import de.htwg.se.Hero.model.Player
+
 import org.scalatest._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class HeroSpec extends WordSpec with Matchers {
+
+    "A Player" when { "new" should {
+        val player = Player("Test")
+        "have a name"  in {
+            player.name should be("Test")
+        }
+        "have a nice String representation" in {
+            player.toString should be("Test")
+        }
+    }}
+
     "At start of the game," when { "first text appear" should {
         val start = gameName()
         "it show the title"  in {
